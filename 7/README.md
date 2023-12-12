1c5b36b34bd0a4764687a524da92efc96488269c
Roland Kajatin <roland.kajatin@gmail.com>
2023-12-12 08:01:55 +0100

    solved day 7

---
+++ 7/README.md
@@ -0,0 +1,8 @@
+# Day 7: Swift
+
+Puzzles:  <https://adventofcode.com/2023/day/7>
+
+```bash
+swiftc puzzle_1.swift -o puzzle_1 && ./puzzle_1
+swiftc puzzle_2.swift -o puzzle_2 && ./puzzle_2
+```
---
+++ 7/puzzle_2.swift
@@ -0,0 +1,112 @@
+import Foundation
+
+struct Strength {
+    let value: Int
+    let name: String
+
+    init(cards: [Card]) {
+        let letters = cards.map({ $0.letter })
+        var letterCounts = [String: Int]()
+        for letter in letters {
+            letterCounts[letter, default: 0] += 1
+        }
+
+        // Correct to five of a kind
+        if (letterCounts.keys.contains("J") && letterCounts.keys.count <= 2) || letterCounts.values.contains(5) {
+            self.value = 7
+            self.name = "Five of a kind"
+        } else if letterCounts.filter({ $0.key != "J" }).values.contains(letters.count - letterCounts["J", default: 0] - 1) || letterCounts.values.contains(4) {
+            self.value = 6
+            self.name = "Four of a kind"
+        } else if (letterCounts.keys.count == 3 && letterCounts["J", default: 0] >= 1) || (letterCounts.keys.count == 2 && letterCounts["J", default: 0] >= 2) || (letterCounts.values.contains(3) && letterCounts.values.contains(2)) {
+            self.value = 5
+            self.name = "Full house"
+        } else if letterCounts["J", default: 0] >= 2 || (letterCounts.values.contains(2) && letterCounts["J", default: 0] >= 1) || letterCounts.values.contains(3) {
+            self.value = 4
+            self.name = "Three of a kind"
+        } else if letterCounts.values.filter({ $0 == 2 }).count == 2 || (letterCounts.values.filter({ $0 == 2 }).count == 1 && letterCounts["J", default: 0] >= 1) || letterCounts["J", default: 0] >= 2 {
+            self.value = 3
+            self.name = "Two pairs"
+        } else if letterCounts.values.contains(2) || letterCounts["J", default: 0] >= 1 {
+            self.value = 2
+            self.name = "One pair"
+        } else {
+            self.value = 1
+            self.name = "High card"
+        }
+    }
+}
+
+struct Card {
+    let value: Int
+    let letter: String
+
+    static func parse_value(_ letter: String) -> Int {
+        switch letter {
+        case "A":
+            return 14
+        case "K":
+            return 13
+        case "Q":
+            return 12
+        case "J":
+            return 1
+        case "T":
+            return 10
+        default:
+            return Int(letter)!
+        }
+    }
+}
+
+struct Hand {
+    let bid: Int
+    let cards: [Card]
+    let strength: Strength
+}
+
+var hands: [Hand] = []
+
+if let filePath = Bundle.main.path(forResource: "puzzle_input", ofType: "txt") {
+    do {
+        let contents = try String(contentsOfFile: filePath)
+
+        let handsStringArray = contents.components(separatedBy: "\n")
+        for handString in handsStringArray {
+            let handStringArray = handString.components(separatedBy: " ")
+            var cards: [Card] = []
+            for cardString in handStringArray[0] {
+                let value = Card.parse_value(String(cardString))
+                let letter = String(cardString)
+                cards.append(Card(value: value, letter: letter))
+            }
+            let bid = Int(handStringArray[1])!
+            let strength = Strength(cards: cards)
+            hands.append(Hand(bid: bid, cards: cards, strength: strength))
+        }
+    } catch {
+        print("Error reading file: \(error.localizedDescription)")
+    }
+}
+
+func handSorting(_ lhs: Hand, _ rhs: Hand) -> Bool {
+    if (lhs.strength.value == rhs.strength.value) {
+        for i in 0..<lhs.cards.count {
+            if (lhs.cards[i].value == rhs.cards[i].value) {
+                continue
+            } else {
+                return lhs.cards[i].value < rhs.cards[i].value
+            }
+        }
+    }
+
+    return lhs.strength.value < rhs.strength.value
+}
+
+hands.sort(by: { handSorting($0, $1) })
+var summa = 0
+for (index, hand) in hands.enumerated() {
+    summa += hand.bid * (index + 1)
+    // print("\(index + 1): \(hand.bid) * \(index + 1) = \(hand.bid * (index + 1)) (\(hand.cards.map({ $0.letter }).joined(separator: "")) \(hand.strength.name))")
+}
+print("Day 7 Puzzle 2: \(summa)")
